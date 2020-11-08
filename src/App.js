d7cd8a9350768d1e0ad2b2ca943ef24328b46dca
kartik <kartikey.sri98@gmail.com>
2020-11-08 11:03:22 +0530

    initial layout finished

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import "./styles/App.scss"
+import "./styles/App.scss";
+import Player from "./components/Player";
+import Song from "./components/Song";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Song/>
+      <Player/>
     </div>    
   );
 }
---
+++ src/components/Song.js
@@ -0,0 +1,9 @@
+function Song() {
+  return (
+    <div className="song-container">
+      
+    </div>
+  )
+}
+
+export default Song;
